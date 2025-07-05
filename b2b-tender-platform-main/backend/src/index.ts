e7ade1882da275bbad78a6ef8d11a41a9b0d56b3
Vishal Jituri <25f1002229@ds.study.iitm.ac.in>
2025-07-05 18:57:20 +0530

    Update index.ts

--- b2b-tender-platform-main/backend/src/index.ts
+++ b2b-tender-platform-main/backend/src/index.ts
@@ -33,9 +33,6 @@ if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 app.use("/uploads", express.static(uploadDir));
 
 const allowedOrigins = [
-  "https://b2b-tender-platform.vercel.app",
-  "https://b2b-tender-platform-vishal-jituris-projects.vercel.app",
-  "https://b2b-tender-platform-czptichiv-vishal-jituris-projects.vercel.app",
   "https://b2b-tender-platform-vishaljituri.onrender.com",
   "http://localhost:3000"
 ];
